06d4c6417599a5b8a14e0d21f74d9aa46c312984
ArielXT <arielzaratext@gmail.com>
2022-05-11 23:14:45 -0500

    Agregar archivos de proyecto.

---
+++ CSSDemo/CSSDemo/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CSSDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CSSDemoPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
